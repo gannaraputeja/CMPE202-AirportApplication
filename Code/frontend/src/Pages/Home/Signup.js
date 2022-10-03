d27a23ef3dc1005fc9fe674ba7e0edc539dba089
Spartan <spartan@IMS-011MBA.local>
2022-10-03 11:04:09 -0800

    Signup

--- Code/frontend/src/Pages/Home/Signup.js
+++ Code/frontend/src/Pages/Home/Signup.js
@@ -32,10 +32,6 @@ const Signup = () =>{
                                 {/* <div class="col usernameclass">Hi {username} 👋</div> */}
                             </div>
                             Auth-form {
-                            width: 420px;
-                            box-shadow: rgb(0 0 0 / 16%) 1px 1px 10px;
-                            padding-top: 30px;
-                            padding-bottom: 20px;
                             border-radius: 8px;
                             background-color: white;
                         }
