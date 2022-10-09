c75c8cf2b12c297c5353e8b8c01f2078b9495238
raviteja <gannaraputeja@gmail.com>
2022-10-08 20:19:48 -0800

    Added react changes

---
+++ Code/frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home/Home';
+import HomePage from './Pages/Home/HomePage';
+import LoginPage from './Pages/Home/LoginPage';
+import SchedulePage from './Pages/Home/SchedulePage';
+import Gateway from './Pages/Home/Gateway';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="blur" style= {{top: '-18%', right: '0'}}></div>
+        <div className="blur" style= {{top: '37%', left: '-8rem'}}></div>
+        <Routes>
+          <Route path='/' element={ <HomePage /> } />
+          <Route path="/LoginPage" element={<LoginPage />}></Route>
+          <Route path="/SchedulePage" element={<SchedulePage />}></Route>
+          <Route path="/Gateway" element={<Gateway />}></Route>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
