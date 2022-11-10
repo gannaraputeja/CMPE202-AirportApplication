64db13ecbf3e011ac4d39122a5a3be6e91e35c47
Dasaradh Gutta <dasaradh.gutta@gmail.com>
2022-11-09 22:41:38 -0800

    Schedule

--- Code/frontend/src/Pages/Home/Schedule.js
+++ Code/frontend/src/Pages/Home/Schedule.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useState,useEffect } from 'react';
+import backendurl from "./backendUrl";
 
 
-const Gate = () =>{
+const Schedule = () =>{
 
 
     const [username,setUsername] = useState('');
@@ -29,6 +30,28 @@ const Gate = () =>{
     const handleRole= event =>{
         console.log("ROLE VAL:::",event.target.value);
         window.sessionStorage.setItem("Role",event.target.value);
+    }
+    const getAirportSchedule = () =>{
+        Axios.get(`${backendurl}/airport-schedules/${hours}`,)
+            .then((response) => {
+                console.log("AAAA:",response.data);
+                setAirportSchedule(response.data);
+            })
+            .catch(err => {
+                console.log(err.response);
+            });
+    }
+
+    const postBaggage = () =>{
+        Axios.post(`${backendurl}/airport/assign/baggage-carousel`)
+            .then((response) =>{
+                console.log("Success:",response);
+                alert("Successfully Assigned Baggage 👍");
+                getAirportSchedule();
+            })
+            .catch(err => {
+                console.log(err.response);
+            })
     }
 
     const validate = () => {
@@ -61,4 +84,4 @@ const Gate = () =>{
     )
 }
 
-export default Gate;
+export default Schedule;
