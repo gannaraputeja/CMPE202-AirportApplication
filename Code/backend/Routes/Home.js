0b61b1806c63253596c47eff605cd7b9a3204e84
Raviteja Gannarapu <gannaraputeja@gmail.com>
2022-11-19 00:21:22 -0800

    airport schedule with respect to UTC arrivaltime

--- Code/backend/Routes/Home.js
+++ Code/backend/Routes/Home.js
@@ -1,18 +1,31 @@
-
+import express from 'express'
 import { AirportSchedule } from '../Models/index.js';
+import { Op } from 'sequelize'
+
+const router = express.Router();
 
 router.get('/', async (req, res) => {
     res.send('Welcome Home Page')
 })
 
-router.get("/airportschedule/:id", (req, res) => {
-        AirportSchedule.findAll({
-            where: {id: req.params.id}
-        })
-        .then(schedule => {
-            res.json(schedule[0]);
-        })
-        .catch(err => console.error(`Error while retrieving flight schedule`, err.message))
+router.get("/airportschedule/:id", async (req, res) => {    
+        try{
+            var startDate = new Date();
+            var endDate = new Date(new Date().getTime() + req.params.id*60*60*1000);
+            console.log(startDate);
+            console.log(endDate);
+            const response = await AirportSchedule.findAll({
+                where: {
+                    arrivalTime:{
+                        [Op.between] : [ startDate, endDate]
+                    }
+                }
+            })
+            return res.status(200).json(response);    
+        }
+        catch(err){ 
+            res.status(400).json(`Error while retrieving flight schedule`);
+        }
 });
 
 export default router
