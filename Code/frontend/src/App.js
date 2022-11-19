4d40c92bf526490aeef226051799b9e2991c684f
M.V.N.S.Prudhvi <34057003+prudhvimvns@users.noreply.github.com>
2022-11-19 11:33:53 -0800

    Login Page commit

--- Code/frontend/src/App.js
+++ Code/frontend/src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home';
 import HomePage from './Pages/Home/HomePage';
-
+import LoginPage from './Pages/Home/LoginPage';
 function App() {
   return (
     <div className="App">
@@ -11,6 +11,7 @@ function App() {
         <div className="blur" style= {{top: '37%', left: '-8rem'}}></div>
         <Routes>
           <Route path='/' element={ <HomePage /> } />
+          <Route path="/LoginPage" element={<LoginPage />}></Route>
         </Routes>
     </div>
   );
