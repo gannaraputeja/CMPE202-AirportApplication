8caf8af5f7898d68fca8ebe8f6963f8de6988820
Raviteja Gannarapu <gannaraputeja@gmail.com>
2022-11-19 22:42:23 -0800

    Added airline route for adding schedule

---
+++ Code/backend/Controllers/AirlineController.js
@@ -0,0 +1,22 @@
+import { FlightInstance } from "../Models/index.js";
+
+export const addFlightSchedule = async(req, res) =>{
+    try{
+        const flightData = await FlightInstance.create({
+            status: req.body.status,
+            departureTime: req.body.departureTime,
+            arrivalTime: req.body.arrivalTime,
+            origin: req.body.origin,
+            destination: req.body.destination,
+            flightId: req.body.flightId
+        });
+
+        console.log(flightData);
+
+        res.status(201).send(flightData);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).json({message: "Airline flights schedule could not be added"});
+    }
+};
--- Code/backend/Routes/index.js
+++ Code/backend/Routes/index.js
@@ -1,9 +1,11 @@
 import HomeRoute from './HomeRoute.js'
 import AirportRoute from './AirportRoute.js'
+import AirlineRoute from './AirlineRoute.js'
 
 const Routes = (app) => {
     app.use('/', HomeRoute)
     app.use('/airport', AirportRoute)
+    app.use('/airline', AirlineRoute)
 }
 
 export default Routes
