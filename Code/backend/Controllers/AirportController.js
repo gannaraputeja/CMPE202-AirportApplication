7cdc46e6eea8317261f9fa379035bc27024dd407
Raviteja Gannarapu <gannaraputeja@gmail.com>
2022-11-20 01:59:54 -0800

    gate assign code changes to find terminal id and assign gate

--- Code/backend/Controllers/AirportController.js
+++ Code/backend/Controllers/AirportController.js
@@ -73,18 +73,27 @@ export const assignBaggageCarousel = async (req, res) => {
     }
 }
 
-export const gateAssignment = async(req, res) =>{
+export const gateAssignment = async (req, res) =>{
     const t = await db.sequelize.transaction();
     try{
         if(req.params.flightInstanceId.trim() ==='' || isNaN(req.params.flightInstanceId))
-            res.status(400).json("Invalid flight instance Id, Flight instance should be a number...");
+            return res.status(400).json("Invalid flight instance Id, Flight instance should be a number...");
+
+        const scheduleData = await AirportSchedule.findOne({
+            where: {
+                flightInstanceId: req.params.flightInstanceId
+            }
+        });
 
         const allGates = await Gate.findAll({
-            where:{ status: 'active'}
+            where:{ 
+                status: 'active',
+                terminalId: scheduleData.terminalId
+            }
         });
 
         if(allGates.length==0)
-            res.status(400).json({message: "Airport is busy. No gates available for landing...."})
+            return res.status(400).json({message: "Airport is busy. No gates available for landing...."})
 
         const randomNumber = Math.floor(Math.random()* (allGates.length));
 
