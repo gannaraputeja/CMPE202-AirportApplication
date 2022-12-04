6bbbf982cbddfbc30ef3ef0459994d83f8925bc7
jayaprakash-06 <jayaprakashreddy.pachika@sjsu.edu>
2022-12-03 17:46:32 -0800

    Added updateGateStatus API to activate/inactivate specific gate.

--- Code/backend/Routes/AirportRoute.js
+++ Code/backend/Routes/AirportRoute.js
@@ -1,11 +1,18 @@
 import express from 'express'
-import {assignBaggageCarousel, gateAssignment, getGates, updateGatesStatus} from "../Controllers/AirportController.js"
+import {
+    assignBaggageCarousel,
+    gateAssignment,
+    getGates,
+    updateGatesStatus,
+    updateGateStatus
+} from "../Controllers/AirportController.js"
 import {AuthMiddleware, IsAirportUser} from "../Middleware/AuthMiddleware.js";
 
 const router = express.Router()
 
-router.post('/update/gates', AuthMiddleware, IsAirportUser, updateGatesStatus)
-router.post('/assign/baggageCarousel', AuthMiddleware, IsAirportUser, assignBaggageCarousel)
+router.put('/update/gates', AuthMiddleware, IsAirportUser, updateGatesStatus);
+router.post('/assign/baggageCarousel', AuthMiddleware, IsAirportUser, assignBaggageCarousel);
 router.post("/assignGate/:flightInstanceId", AuthMiddleware, IsAirportUser, gateAssignment);
-router.get('/get/gates', getGates);
+router.get('/get/gates', AuthMiddleware, IsAirportUser, getGates);
+router.put('/update/gate/:id', AuthMiddleware, IsAirportUser, updateGateStatus);
 export default router
